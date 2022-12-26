e68d1dd8b9c353b02289d876d981c1068fab17c4
Stan-Soponaru Ioan-Alexandru <102477559+XCriwn@users.noreply.github.com>
2022-12-26 23:05:24 +0200

    API Part 16 (city) (#4)
    * API Part 16 (city)
    
    * Moved the {
    
    Co-authored-by: Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
    
    Co-authored-by: Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>

---
+++ source/v1/routes/city.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import controller from "../controllers/city";
+const router = express.Router();
+
+router.get("/search", controller.searchCity);
+router.get("/", controller.getCity);
+router.post("/", controller.addCity);
+router.delete("/", controller.deleteCity);
+
+export = router;
