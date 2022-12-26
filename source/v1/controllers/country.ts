36acaf3c1e134017b72c1b7afbbe8955a3490128
Chrisharris1001 <81560252+Chrisharris1001@users.noreply.github.com>
2022-12-26 23:20:45 +0200

    API Part 15 (country) (#5)
    * API Part 15 (country)
    
    * Apply suggestions from code review
    
    Co-authored-by: Stan-Soponaru Ioan-Alexandru <102477559+XCriwn@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    remove ```
    
    Co-authored-by: Stan-Soponaru Ioan-Alexandru <102477559+XCriwn@users.noreply.github.com>
    Co-authored-by: Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>

---
+++ source/v1/controllers/country.ts
@@ -0,0 +1,95 @@
+import {Request, Response, NextFunction} from "express";
+import {query} from "../../database";
+import Country from "../interfaces/Country";
+
+const getCountries = (req: Request, res: Response, next: NextFunction) => {
+    query('SELECT * FROM COUNTRY')
+        .then((result: any) => res.status(200).json(result as Country[]))
+        .catch((err: any) => {
+        return res.status(500).json({
+            message: 'Server error',
+            error: err
+        });
+    });
+}
+
+const getCountry = (req: Request, res: Response, next: NextFunction) => {
+    const id = req.query.id;
+    if (!id) {
+        return res.status(400).json({
+            message: 'Bad request'
+        });
+    }
+    query('SELECT * FROM COUNTRY WHERE id = ?', [id])
+        .then((result: any) => {
+            if (result.length == 0) {
+                res.status(404).json({
+                    message: 'Country not found'
+                });
+            } else {
+                res.status(200).json(result as Country[]);
+            }
+        });
+}
+const searchCountry = (req: Request, res: Response, next: NextFunction) => {
+    //search by name or icao
+    const name = req.query.name;
+    const icao = req.query.icao;
+    if (!name && !icao) {
+        return res.status(400).json({
+            message: 'Bad request'
+        });
+    }
+    query('SELECT * FROM COUNTRY WHERE name LIKE ? OR icao LIKE ?', [`%${name}%`, `%${icao}%`])
+        .then((result: any) => {
+            if (result.length == 0) {
+                res.status(404).json({
+                    message: 'Country not found'
+                });
+            } else {
+                res.status(200).json(result as Country[]);
+            }
+        });
+}
+const addCountry = (req: Request, res: Response, next: NextFunction) => {
+    const icao = req.body.icao;
+    const name = req.body.name;
+    if (!icao || !name) {
+        return res.status(400).json({
+            message: 'Country ICAO and name are required'
+        });
+    }
+    //icao must have only letters and be 3 characters long
+    if (!/^[A-Z]{3}$/.test(icao)) {
+        return res.status(400).json({
+            message: 'Invalid ICAO code'
+        });
+    }
+    query('INSERT INTO COUNTRY(icao, name) VALUES (?, ?)', [icao, name])
+        .then((result: any) => res.status(201).json({message: 'Country created'}))
+        .catch((err: any) => {
+        return res.status(500).json({
+            message: 'Server error',
+            error: err
+        });
+    });
+}
+
+const deleteCountry = (req: Request, res: Response, next: NextFunction) => {
+    const id = req.query.id;
+    if (!id) {
+        return res.status(400).json({
+            message: 'Country ID is required'
+        });
+    }
+    query('DELETE FROM COUNTRY WHERE id = ?', [id])
+        .then((result: any) => res.status(200).json({message: 'Country deleted'}))
+        .catch((err: any) => {
+        return res.status(500).json({
+            message: 'Server error',
+            error: err
+        });
+    });
+}
+
+export default {getCountries, getCountry, searchCountry, addCountry, deleteCountry};
---
+++ source/v1/routes/country.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import controller from '../controllers/country';
+const router = express.Router();
+
+router.get('/all', controller.getCountries);
+router.get('/', controller.getCountry);
+router.get('/search/', controller.searchCountry);
+router.post('/', controller.addCountry);
+router.delete('/', controller.deleteCountry);
+
+export = router;
