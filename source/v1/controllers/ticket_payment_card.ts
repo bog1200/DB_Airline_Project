a6a9b9ec1d36aade54f93a4237bcf06e65f6f9c2
Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
2022-12-30 23:18:45 +0200

    Apply suggestions from code review

--- source/v1/controllers/ticket_payment_card.ts
+++ source/v1/controllers/ticket_payment_card.ts
@@ -4,40 +4,6 @@ import TicketPaymentCard from "../interfaces/TicketPaymentCard";
 /**
  * @openapi
  * paths:
- *     /ticket/payment/cards/all:
- *         get:
- *             summary: Get all ticket payment cards
- *             tags:
- *                 - payments
- *             responses:
- *                 '200':
- *                     description: A ticket payment card object
- *                     content:
- *                         application/json:
- *                             schema:
- *                                 $ref: '#/components/schemas/TicketPaymentCard'
- *                 '500':
- *                     description: Server error
- *                     content:
- *                         application/json:
- *                             schema:
- *                                 $ref: '#/components/schemas/Error'
- */
-const getTicketPaymentsCard = async (req: Request, res: Response) => {
-    const result: RowDataPacket[] = <RowDataPacket[]> await query("SELECT * FROM TICKET_PAYMENT_CARD");
-    if (result.length > 0) {
-        return res.status(200).json({
-            message: "Ticket Payment Card found",
-            data: result as TicketPaymentCard[]
-        });
-    }
-    return res.status(500).json({
-        message: 'Server Error'
-    });
-}
-/**
- * @openapi
- * paths:
  *     /ticket/payment/cards:
  *         get:
  *             summary: Get ticket payment card by id
@@ -97,7 +63,7 @@ const getTicketPaymentCard= async (req: Request, res: Response) => {
  *                 name: ticket_id
  *                 schema:
  *                    type: string
- *                    description: The ticket_id of the ticket payment card\
+ *                    description: The ticket id of the ticket payment card
  *               - in: query
  *                 name: card_id
  *                 schema:
@@ -135,9 +101,6 @@ const searchTicketPaymentCard = async (req: Request, res: Response) => {
     let sql = "SELECT * FROM TICKET_PAYMENT_CARD WHERE ";
     let params : any[] = [];
     if(ticket_id){
-        if(params.length > 0){
-            sql += " AND ";
-        }
         sql += "TICKET_ID = ?";
         params.push(ticket_id);
     }
@@ -205,7 +168,7 @@ const addTicketPaymentCard = async (req: Request, res: Response) => {
     if(ticketPaymentCheck.length == 0){
         return res.status(400).json({
             message: 'Bad Request',
-            error: 'Ticket Payment Card not found'
+            error: 'Ticket not found'
         });
     }
     const AccountCardCheck: RowDataPacket[] = <RowDataPacket[]> await query("SELECT * FROM ACCOUNT_CARD WHERE id = ?", [TicketPaymentCard.card_id]);
