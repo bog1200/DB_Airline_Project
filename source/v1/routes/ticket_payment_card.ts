c9e67b07148df15f5e9eff23794d37eead302d48
Cristea Razvan <81560252+Chrisharris1001@users.noreply.github.com>
2022-12-30 22:58:14 +0200

    API (ticket_payment_card + documentation)

--- source/v1/routes/ticket_payment_card.ts
+++ source/v1/routes/ticket_payment_card.ts
@@ -2,7 +2,6 @@ import express from "express";
 import controller from "../controllers/ticket_payment_card";
 const router = express.Router();
 
-router.get("/all", controller.getTicketPaymentsCard);
 router.get("/", controller.getTicketPaymentCard);
 router.get("/search", controller.searchTicketPaymentCard);
 router.post("/", controller.addTicketPaymentCard);
