97bed575365268f613218d00feab62b1a8200586
Chrisharris1001 <81560252+Chrisharris1001@users.noreply.github.com>
2022-12-30 23:23:45 +0200

    API (ticket_payment_card + documentation) (#16)

---
+++ source/v1/routes/ticket_payment_card.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import controller from "../controllers/ticket_payment_card";
+const router = express.Router();
+
+router.get("/", controller.getTicketPaymentCard);
+router.get("/search", controller.searchTicketPaymentCard);
+router.post("/", controller.addTicketPaymentCard);
+router.delete("/", controller.deleteTicketPaymentCard);
+
+export = router;
